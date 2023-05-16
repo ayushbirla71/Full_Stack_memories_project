e11daa2d0bb6f65be1f71cc55d8b55af66b7d643
ayushbirla71 <114582571+ayushbirla71@users.noreply.github.com>
2023-05-16 15:54:29 +0530

    client

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://ayush8120:GeGo5qhr7wM6VQyg@cluster0.n1nevi5.mongodb.net/Memories_app?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
